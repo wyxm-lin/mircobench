32f7e7dca88536a87f90eb0e4cd5556558fbec56
wyxm <3460437682@qq.com>
2024-05-02 13:45:37 +0000

    gitignore

---
+++ build.sh
@@ -0,0 +1,2 @@
+g++ -O2 -std=c++11 -o server server.cc -I ../../eRPC/src -I ../../eRPC/third_party/asio/include -L ../../eRPC/build -lerpc -lpthread -lnuma -ldl -libverbs -DERPC_INFINIBAND=true
+g++ -O2 -std=c++11 -o client client.cc -I ../../eRPC/src -I ../../eRPC/third_party/asio/include -L ../../eRPC/build -lerpc -lpthread -lnuma -ldl -libverbs -DERPC_INFINIBAND=true
